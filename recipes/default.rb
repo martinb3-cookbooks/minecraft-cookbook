626288e21b5781e65acadb9453170a2ade5fd737
Martin Smith <martin@mbs3.org>
2015-01-01 09:27:45 -0600

    Fixup iptables to be correct

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,16 +12,16 @@ include_recipe 'mineos'
 node.set_unless['iptables']['rules'] = []
 
 rule25565 = {
-  name: "port_25565",
+  name: "port_25565_udp",
   value: "-A FWR -p udp -m udp --dport 25565 -j ACCEPT"
 }
 node.set['iptables']['rules'] << rule25565 if node['iptables']['rules'].select { |r| r[:name] == rule25565[:name] }.empty?
 
-ports = [8080]
+ports = [8080, 25565]
 
 ports.each do |p|
   rule = {
-    name: "port_#{p}",
+    name: "port_#{p}_tcp",
     value: "-A FWR -p tcp -m tcp --dport #{p} -j ACCEPT"
   }
   node.set['iptables']['rules'] << rule if node['iptables']['rules'].select { |r| r[:name] == rule[:name] }.empty?
